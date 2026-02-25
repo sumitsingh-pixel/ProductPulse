a8795dac615e84fe71a03ecfe1b0c8e3262af6e8
sumitsingh-pixel <sumit.singh@materialplus.io>
2026-02-25 13:36:56 +0530

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,21 @@
 
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = 'https://vknlidvgdspllrvaetjy.supabase.co';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrbmxpZHZnZHNwbGxydmFldGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTYyNDcsImV4cCI6MjA4NTgzMjI0N30.iKL5ykyTW5okwt4AuTssBjE_YmtKoAHE53lWbno9ZuU';
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://vknlidvgdspllrvaetjy.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrbmxpZHZnZHNwbGxydmFldGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTYyNDcsImV4cCI6MjA4NTgzMjI0N30.iKL5ykyTW5okwt4AuTssBjE_YmtKoAHE53lWbno9ZuU';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+});
